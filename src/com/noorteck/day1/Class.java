2b1358cfbf761757c39a5f3229bc33285993c4bf
Admin <Admin@DESKTOP-MS4SSFE.home>
2023-01-10 20:51:03 -0600

    next

---
+++ src/com/noorteck/day1/Class.java
@@ -0,0 +1,10 @@
+package com.noorteck.day1;
+
+public class Class {
+
+	public static void main(String [] args) {
+		System.out.println("yeeeeeeeeeeeeeeeted");
+		System.out.println("cool");
+		
+	}
+}
